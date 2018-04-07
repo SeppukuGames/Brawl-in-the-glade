cafa53612bf902bdf61d7092b899ac13c8ae9987
adralcan <adralcan@ucm.es>
2018-04-07 20:27:18 +0200

    Otra implementación de Mensajería, mejor que la anterior

--- Src/Application/Messaging.h
+++ Src/Application/Messaging.h
@@ -2,48 +2,103 @@
 
 #include <vector> // Para almacenar la lista de senders y listeners
 
-// Esto va a ser un tipo especifico de mensajes y queremos tener diferentes tipos
-struct VectorMessage {
-	// Es un area bidimensional que determina a quien va el mensaje
-	// Es un punto y una direccion
-	// Sirve para proyectar nuestra clase y "disparar un proyectil",
-	// a partir de un punto (x1, y1) en la dirección (x2, y2)
-	float x1;
-	float x2;
-	float y1;
-	float y2;
+// // Esto va a ser un tipo especifico de mensajes y queremos tener diferentes tipos
+// struct VectorMessage {
+// 	// Es un area bidimensional que determina a quien va el mensaje
+// 	// Es un punto y una direccion
+// 	// Sirve para proyectar nuestra clase y "disparar un proyectil",
+// 	// a partir de un punto (x1, y1) en la dirección (x2, y2)
+// 	float x1;
+// 	float x2;
+// 	float y1;
+// 	float y2;
+// };
+// 
+// // Las dos siguientes clases tienen el nombre de VectorX para que sirva en caso de que
+// // creemos distintos tipos de emisores y receptores
+// 
+// class VectorSender {
+// 
+// public:
+// 	VectorSender();
+// 	~VectorSender();
+// 
+// 	void SendMessage(float x1, float y1, float x2, float y2); // Podemos pasar por parametro un VectorMessage message
+// 															  // Si ponemos eso, hay que asegurarse de haber construido
+// 															  // el mensaje para despues enviarlo.
+// 															  // La clase Message construira el mensaje por sí misma.
+// 	void recieveListener(VectorListener * listener);
+// 
+// private:
+// 	std::vector<VectorListener*> mListeners; // Vector de miembros que son listeners
+// };
+// 
+// class VectorListener {
+// 
+// public:
+// 	VectorListener();
+// 	~VectorListener();
+// 
+// 	void listenToSender(VectorSender * sender);
+// 	std::vector<float> GetMessage(); // devuelve el primer mensaje (las dos coordenadas) de la pila
+// 	void recieveMessage(VectorMessage newMessage);
+// 
+// private:
+// 	std::vector<VectorMessage> mMessages;
+// };
+
+enum TipoMensaje {
+	MSG_FISICO,
+	MSG_LOGICO
 };
 
-// Las dos siguientes clases tienen el nombre de VectorX para que sirva en caso de que
-// creemos distintos tipos de emisores y receptores
-
-class VectorSender {
-
+class Mensaje {
 public:
-	VectorSender();
-	~VectorSender();
-
-	void SendMessage(float x1, float y1, float x2, float y2); // Podemos pasar por parametro un VectorMessage message
-															  // Si ponemos eso, hay que asegurarse de haber construido
-															  // el mensaje para despues enviarlo.
-															  // La clase Message construira el mensaje por sí misma.
-	void recieveListener(VectorListener * listener);
-
-private:
-	std::vector<VectorListener*> mListeners; // Vector de miembros que son listeners
+	TipoMensaje m_TipoMensaje;
+	int m_Id;
+	Mensaje(TipoMensaje tipo, int id) : m_TipoMensaje(tipo), m_Id(id) {
+	
+	}
+	~Mensaje();
 };
 
-class VectorListener {
+class MensajeFisico : public Mensaje {
+public:
+	MensajeFisico(int id) : Mensaje(MSG_FISICO, id) {
+
+	}
+	~MensajeFisico();
+};
+
+class MensajeLogico : public Mensaje {
+public:
+	MensajeLogico(int id) : Mensaje(MSG_LOGICO, id) {
+
+	}
+	~MensajeLogico();
+};
+
+class BaseDeMensajes {
+	// Voy a definir un tipo que puede ser cualquier cosa en memoria
+	typedef void(*Encargado)(BaseDeMensajes*, const Mensaje&);
+
+	struct Union {
+		TipoMensaje m_tipo;
+		BaseDeMensajes * m_Observer;
+		BaseDeMensajes * m_Observed;
+		Encargado m_Encargado;
+		Union(TipoMensaje tipo, BaseDeMensajes * Observer,
+			BaseDeMensajes * Observed, Encargado Encargado) :
+			m_tipo(tipo), m_Observer(Observer),
+			m_Observed(m_Observed), m_Encargado(Encargado) {
+
+		}
+	};
+
+	std::vector<Union> m_Uniones;
 
 public:
-	VectorListener();
-	~VectorListener();
-
-	void listenToSender(VectorSender * sender);
-	std::vector<float> GetMessage(); // devuelve el primer mensaje (las dos coordenadas) de la pila
-	void recieveMessage(VectorMessage newMessage);
-
-private:
-	std::vector<VectorMessage> mMessages;
+	BaseDeMensajes();
+	~BaseDeMensajes();
 };
 
