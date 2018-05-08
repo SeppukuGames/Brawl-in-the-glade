da49c1fcf3566d3ad4ab191f3e9d200a4675ff3f
adralcan <adrian97ad@gmail.com>
2018-05-08 19:28:37 +0200

    Fallo tonto que está en todo el juego, corregido para que vayan los dos inputs de ratón

--- Src/Application/BaseApplication.cpp
+++ Src/Application/BaseApplication.cpp
@@ -510,21 +510,21 @@ bool BaseApplication::keyReleased(const OIS::KeyEvent &arg)
 
 bool BaseApplication::mouseMoved(const OIS::MouseEvent &arg)
 {
-	for (int i = 0; i < keyInputObservers.size(); i++)
+	for (int i = 0; i < mouseInputObservers.size(); i++)
 		mouseInputObservers[i]->mouseMoved(arg);
 	return true;
 }
 
 bool BaseApplication::mousePressed(const OIS::MouseEvent &arg, OIS::MouseButtonID id)
 {
-	for (int i = 0; i < keyInputObservers.size(); i++)
+	for (int i = 0; i < mouseInputObservers.size(); i++)
 		mouseInputObservers[i]->mousePressed(arg, id);
 	return true;
 }
 
 bool BaseApplication::mouseReleased(const OIS::MouseEvent &arg, OIS::MouseButtonID id)
 {
-	for (int i = 0; i < keyInputObservers.size(); i++)
+	for (int i = 0; i < mouseInputObservers.size(); i++)
 		mouseInputObservers[i]->mouseReleased(arg, id);
 	return true;
 }
